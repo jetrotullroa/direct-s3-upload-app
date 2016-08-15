7935acf5140a50243e5b555703248695daf545ed
jetrotullroa <jetrotullroa@gmail.com>
2016-08-15 10:17:49 +0800

    initial commit - setup Posts model view controller. setup gems and dependencies

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ActiveRecord::Base
+
+  has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  has_many :attachments, as: :attachable
+  has_many :images, as: :imageable
+
+end
